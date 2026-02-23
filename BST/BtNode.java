d51177b54b4c8ad1295ef54ee36672bf501a9f98
Guilherme <macarioguilherme8534@gmail.com>
2026-02-23 18:35:49 -0300

    Commit!

--- BST/BtNode.java
+++ BST/BtNode.java
@@ -42,52 +42,5 @@ public class BtNode<T> {
     public void setParent(BtNode<T> parent) {
         this.parent = parent;
     }
-
-    /* ================= MÉTODOS NO PADRÃO RECURSIVO ================= */
-
-    public boolean isEmpty() {
-        throw new UnsupportedOperationException("Escolha isEmptyRecursivo");
-    }
-
-
-    public int height() {
-        throw new UnsupportedOperationException("Escolha heightRecursivo");
-    }
-
-
-    public BtNode<T> search(T element) {
-        throw new UnsupportedOperationException("Escolha searchRecursivo");
-    }
-
-
-    public void insert(T value) {
-        throw new UnsupportedOperationException("Escolha insertRecursivo");
-    }
-
-
-    public void remove(T key) {
-        throw new UnsupportedOperationException("Escolha removeRecursivo");
-    }
-
-
-    public T[] preOrder() {
-        throw new UnsupportedOperationException("Escolha preOrderRecursivo");
-    }
-
-
-    public T[] order() {
-        throw new UnsupportedOperationException("Escolha orderRecursivo");
-    }
-
-
-    public T[] postOrder() {
-        throw new UnsupportedOperationException("Escolha postOrderRecursivo");
-    }
-
-
-    public int size() {
-        throw new UnsupportedOperationException("Escolha sizeRecursivo");
-    }
-
-    
+  
 }
